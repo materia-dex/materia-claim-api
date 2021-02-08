a5a58b38a613ee2bcaaab59080e8750e686a24f1
Mirco Ramundo <mramundo@outlook.it>
2021-02-08 16:59:28 +0100

    Claim APis

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # materia-claim-api
+## Usage
+Clone the repository:
+```shell
+> git clone https://github.com/materia-dex/materia-claim-api.git
+```
+Setup the project:
+```shell
+> cd materia-claim-api
+> npm install
+```
+Launch the server:
+```shell
+> node app.js
+```
+## Swagger
+You can test APIs using Swagger UI configured on [localhost:80](http://localhost:80 "localhost:80").
---
+++ app.js
@@ -0,0 +1,38 @@
+// Express
+const express = require("express");
+const app = express();
+const port = 80;
+
+// API
+const api = require("./api");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpecifications = require("./swagger/swagger.json");
+
+// Load APIs
+api.boot(app);
+
+// Express Configuration
+app.listen(port);
+
+// Expose Swagger
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerSpecifications));
+
+// Internal Error Handler
+app.use(function (error, request, response, next) {
+    response.status(500).send({
+        status: 500,
+        message: "error",
+        data: error.message
+    });
+});
+
+// Not Found Error Handler
+app.use(function (request, response, next) {
+    response.status(404).send({
+        status: 404,
+        message: "error",
+        data: "not found"
+    });
+});
